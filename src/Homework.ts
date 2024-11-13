bc1d946f2bea0ed11789dd88c825d3b35836eea4
Abraham Munoz <abmwc16@gmail.com>
2024-11-12 19:50:10 -0600

    TS homework Complete

--- src/Homework.ts
+++ src/Homework.ts
@@ -1,3 +1,5 @@
+import { access } from "fs";
+
 interface Product {
   name: string;
   category: string;
@@ -60,80 +62,97 @@ const products: Product[] = [
     count: 80,
     price: 55,
   },
+  {
+    name: 'Car',
+    category: 'Toy',
+    count: 3,
+    price: 150,
+  },
+  {
+    name: 'Doll',
+    category: 'Toy',
+    count: 2,
+    price: 150,
+  },
 ];
 
-/* Create a new list with all product multipliying price by 2
-function multipyPrice<T extends Product>(products : Array<T>): Array<T> {
-    const newArray: Array<T> = []
-    for (const product of products){
-        const newPrice = {...product, price: product.price * 2};
-        newArray.push(newPrice);
-    }
+/** 1. Create a new list with all product multipliying price by 2 */
 
-    return newArray;
-};
+// function multipyPrice<T extends Product>(products: Array<T>): Array<T> {
+//   const newArray: Array<T> = [];
+//   for (const product of products) {
+//     const newPrice = {...product, price: product.price * 2};
+//     newArray.push(newPrice);
+//   }
 
-console.log(multipyPrice(products));
-
-// const newArray: Array<Product> = products.map(products => {return{...products, price: products.price * 2}});
-// console.log(newArray);*/
-
-/* Create a function that recevies the category as parameter and filters all products of that specific category
-
-
-function receiveCategory<T extends Product>(param : Array<T>): Array<T> {
-    let newArray = param.filter((a) => a.category === "Fruit");
-    return newArray;
-};
-
-console.log(receiveCategory(products));
-
-// function filterCategory(category: string): Product[] {
-//     return products.filter(product => product.category === category);
+//   return newArray;
 // }
 
-// const vegetable = filterCategory("Vegetable");
-// console.log("Vegetable Products:", vegetable);
+// console.log(multipyPrice(products));
 
-// const fruit = filterCategory("Fruit");
-// console.log("Fruit Products:", fruit);
-*/
+// const newArray: Array<Product> = products.map(products => {
+//   return {...products, price: products.price * 2};
+// });
+// console.log(newArray);
 
+/** 2. Create a function that recevies the category as parameter and filters all products of that specific category */
 /*
-function groupByCategory(products: Product[]) {
-  return products.reduce(
-    (acc: Record<string, Product[]>, product: Product) => {
-      const {category} = product;
-
-      if (!acc[category]) {
-        acc[category] = [];
-      }
-
-      acc[category].push(product);
-      return acc;
-    },
-    {} as Record<string, Product[]>,
-  );
+function receiveCategory<T extends Product>(param: Array<T>): Array<T> {
+  const newArray = param.filter(a => a.category === 'Fruit');
+  return newArray;
 }
 
-console.log(groupByCategory(products));
-*/
+console.log(receiveCategory(products));*/
 
-function groupByCategory(products: Product[]) {
-  return products.reduce(
-    (acc: Record<string, Product[]>, product: Product) => {
-      const {category} = product;
-      const {price} = product;
- 
-      if (!acc[category]) {
-        acc[category] = [];
-      }
+// function filterCategory(category: string): Product[] {
+//   return products.filter(product => product.category === category);
+// }
 
-      acc[category].push(product);
-      return acc;
-    },
-    {} as Record<string, Product[]>,
-  );
+// const vegetable = filterCategory('Vegetable');
+// const fruit = filterCategory('Fruit');
+
+// console.log('Vegetable Products:', vegetable);
+// console.log('Fruit Products:', fruit);
+
+/**3. Create a function that groups products by category  */
+
+// function groupByCategory(products: Product[]) {
+//   return products.reduce(
+//     (acc: Record<string, Product[]>, product: Product) => {
+//       const {category} = product;
+//       console.log(acc);
+//       if (!acc[category]) {
+//         acc[category] = [];
+//       }
+
+//       acc[category].push(product);
+//       return acc;
+//     },
+//     {} as Record<string, Product[]>,
+//   );
+// }
+
+// console.log(groupByCategory(products));
+
+/**4. Create a function that sums the total price of all items in an specific category */
+
+// function sumTotalByCategory(category: string) {
+//   return products
+//     .filter(product => product.category === category)
+//     .reduce((acc, product) => acc + product.price, 0);
+// }
+
+// console.log(`Vegetable total sum is: ${sumTotalByCategory('Vegetable')}$`);
+// console.log(`Fruits total sum is: ${sumTotalByCategory('Fruit')}$`);
+
+/**5. Create a function that returns the names of products which stock is lower than 5 items  */
+
+function lowStock(products: Product[]) {
+  products.forEach(stock => {
+    if (stock.count < 5) {
+      console.log(stock.name);
+    }
+  });
 }
 
-console.log(groupByCategory(products));
+lowStock(products);
--- src/classes.ts
+++ src/classes.ts
@@ -1,90 +1,138 @@
-// data transfer object
-interface PersonInterface {
-  age: number;
-  sayHello: () => string;
-}
+// // data transfer object
+// interface PersonInterface {
+//   age: number;
+//   sayHello: () => string;
+// }
 
-class Human {
+// class Human {
+//   // private name: string;
+
+//   // accesibility modifiers
+//   constructor(
+//     protected name: string,
+//     // protected age: number,
+//   ) {
+//     // this.name = name;
+//   }
+
+//   sayHello(): string {
+//     return 'HELLO!';
+//   }
+// }
+
+// // syntactic suggar
+// // implements
+// class Person extends Human implements PersonInterface {
+//   age: number;
+
+//   private birth: number;
+
+//   constructor(name: string, age: number) {
+//     super(name);
+
+//     this.age = age;
+
+//     this.birth = 2024 - age;
+
+//     // internal usage
+//     console.log(this.constructor.name);
+//   }
+
+//   sayName() {
+//     return this.name;
+//   }
+
+//   getBirth() {
+//     return this.birth;
+//   }
+// }
+
+// // prototype chain
+// // Person.__proto__ = Human;
+
+// const person1 = new Person('Abraham', 30);
+
+// console.log(person1.getBirth());
+
+// // @ts-ignore
+// console.log(person1.__proto__);
+
+// // {}.name {}.algo
+// // {name: patito, };
+// function Patito(name: string) {
+//   //@ts-ignore
+//   this.name = name;
+
+//   function sayName() {
+//     // @ts-ignore
+//     console.log(`Hello my name is patito ${this.name}`);
+//   }
+
+//   // @ts-ignore
+//   this.sayName = sayName;
+// }
+
+// // @ts-ignore
+// Patito.prototype.sayHello = function () {
+//   console.log(this.name);
+// };
+
+// // @ts-ignore
+// Patito.prototype.sayMessage = function (message) {
+//   console.log(`Patito says: ${message}`);
+// };
+
+// // @ts-ignore
+// const p = new Patito('Feo');
+
+// p.sayHello();
+// p.sayName();
+// p.sayMessage("I'm ugly duck");
+
+class Department {
+  // private id: string;
   // private name: string;
+  private employees: string[] = [];
 
-  // accesibility modifiers
   constructor(
-    protected name: string,
-    // protected age: number,
+    private readonly id: string,
+    public name: string,
   ) {
-    // this.name = name;
+    // this.id = id;
+    // this.name = n;
   }
 
-  sayHello(): string {
-    return 'HELLO!';
-  }
-}
-
-// syntactic suggar
-// implements
-class Person extends Human implements PersonInterface {
-  age: number;
-
-  private birth: number;
-
-  constructor(name: string, age: number) {
-    super(name);
-
-    this.age = age;
-
-    this.birth = 2024 - age;
-
-    // internal usage
-    console.log(this.constructor.name);
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
   }
 
-  sayName() {
-    return this.name;
+  addEmployee(employee: string) {
+    this.employees.push(employee);
   }
 
-  getBirth() {
-    return this.birth;
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-// prototype chain
-// Person.__proto__ = Human;
-
-const person1 = new Person('Abraham', 30);
-
-console.log(person1.getBirth());
-
-// @ts-ignore
-console.log(person1.__proto__);
-
-// {}.name {}.algo
-// {name: patito, };
-function Patito(name: string) {
-  //@ts-ignore
-  this.name = name;
-
-  function sayName() {
-    // @ts-ignore
-    console.log(`Hello my name is patito ${this.name}`);
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
   }
-
-  // @ts-ignore
-  this.sayName = sayName;
 }
 
-// @ts-ignore
-Patito.prototype.sayHello = function () {
-  console.log(this.name);
-};
+const it = new ITDepartment('d1', ['Max']);
 
-// @ts-ignore
-Patito.prototype.sayMessage = function (message) {
-  console.log(`Patito says: ${message}`);
-};
+it.addEmployee('Max');
+it.addEmployee('Chris');
 
-// @ts-ignore
-const p = new Patito('Feo');
+// accounting.employees[2] = 'Anna';
 
-p.sayHello();
-p.sayName();
-p.sayMessage("I'm ugly duck");
+it.describe();
+it.printEmployeeInformation();
+// const accountingCopy = {name: 'Peter', describe: accounting.describe};
+
+// accountingCopy.describe();
